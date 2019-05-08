f10ed172250b1885ad781fa47db02e527e200f72
SeanGau <haca00193@gmail.com>
2019-05-08 08:47:36 +0000

    init

--- wikiquery/query_test.py
+++ wikiquery/query_test.py
@@ -2,17 +2,18 @@ from qwikidata.sparql import (get_subclasses_of_item,
                               return_sparql_query_results)
 import json
 
-# send any sparql query to the wikidata query service and get full result back
-# here we use an example that counts the number of humans
 sparql_query = """
-SELECT ?item ?itemLabel ?in
+SELECT ?item ?itemLabel ?itemDescription ?in ?out
 WHERE {
   ?item wdt:P31/wdt:P279* wd:Q55659167;
         wdt:P17 wd:Q865.
   OPTIONAL { ?item wdt:P403 ?in. }
+  OPTIONAL { ?item wdt:P974 ?out. }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "zh-tw, zh-hant, zh, en". }
 }
 """
+#取得位在中華民國的天然水體
+#和他們的上下游(若有)
 res = return_sparql_query_results(sparql_query)
 jsonfile = json.dumps(res)
 fo = open("foo.json", "w")
